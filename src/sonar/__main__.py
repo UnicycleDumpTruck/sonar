6cefcc5ecf3b2ec805010e1a521e61914c1a8cd3
UnicycleDumpTruck <junk@jeffhighsmith.com>
2022-07-19 20:29:46 -0400

    lined rectangles outlining arcs

--- src/sonar/__main__.py
+++ src/sonar/__main__.py
@@ -64,10 +64,12 @@ def main() -> None:
         draw_reticle(screen)
         arc_mgr.draw()
 
-        pygame.draw.arc(screen, GREEN, [HBOX,0,HBOX,BOX], 1.75*PI, PI/4, AWT)
-        pygame.draw.arc(screen, GREEN, [HBOX,0,HBOX/2,BOX], 1.75*PI, PI/4, AWT)
-        pygame.draw.arc(screen, GREEN, [HBOX,0,HBOX/4,BOX], 1.75*PI, PI/4, AWT)
-
+        pygame.draw.arc(
+            screen, GREEN, [HBOX, 0, HBOX, BOX], 1.75*PI, PI/4, AWT)
+        pygame.draw.arc(
+            screen, GREEN, [HBOX, 0, HBOX/2, BOX], 1.75*PI, PI/4, AWT)
+        pygame.draw.arc(
+            screen, GREEN, [HBOX, 0, HBOX/4, BOX], 1.75*PI, PI/4, AWT)
 
         # Flip the display
         pygame.display.flip()
@@ -99,40 +101,34 @@ def draw_reticle(scrn):
 class SoundArc:
     def __init__(self):
         pass
-    
+
 
 class ArcMgr:
-    def reset_arc_gen(self):
+    def reset_empty_ping_arcs(self):
         self.arc_gen_i = (     # Upper right
-            pygame.Rect(HBOX, HBOX-x, x, x)
+            (RED, pygame.Rect(HBOX, HBOX-x, x, x), 0, PI/2,)
             for x in range(ABOXL)
         )
         self.arc_gen_iv = (    # Upper left
-            pygame.Rect(HBOX-x, HBOX-x, 2*x, 2*x)
+            (GREEN, pygame.Rect(HBOX-x, HBOX-x, 2*x, 2*x), PI/2, PI)
             for x in range(ABOXL)
         )
         self.arc_gen_iii = (   # Lower left
-            pygame.Rect(HBOX-x, HBOX, x, x)
+            (RED, pygame.Rect(HBOX-x, HBOX, x, x), PI, 3*PI/2)
             for x in range(ABOXL)
         )
         self.arc_gen_ii = (    # Lower right
-            pygame.Rect(HBOX, HBOX, x, x)
+            (GREEN, pygame.Rect(HBOX, HBOX, x, x), 3*PI/2, 2*PI,)
             for x in range(ABOXL)
         )
 
         self.arc_gen_right = (
-            pygame.Rect(HBOX+x, HBOX-x, x, 2*x)
+            (BLACK, pygame.Rect(HBOX+x, HBOX-x, x, 2*x), 1.75*PI, PI/4,)
             for x in range(ABOXL)
         )
 
         self.arc_gen_left = (
-            pygame.Rect(HBOX-x, HBOX-x, 2*x, 2*x)
-            for x in range(ABOXL)
-        )
-
-
-        self.box_gen_i = (     # Upper right, green
-            pygame.Rect(x, BOX, x, BOX)
+            (BLACK, pygame.Rect(HBOX-x, HBOX-x, 2*x, 2*x), 0.75*PI, 1.25*PI,)
             for x in range(ABOXL)
         )
 
@@ -145,44 +141,39 @@ class ArcMgr:
         self.screen = scrn
         self.pinging = False
         self.biosound = False
-        self.reset_arc_gen()
+        self.arcs = {}
+        self.reset_empty_ping_arcs()
+
+    def draw_single_arc(self, arc_gen):
+        arc_details = next(arc_gen)
+        points = [
+            (arc_details[1][0], arc_details[1][1]),
+            (arc_details[1][0], arc_details[1][1]+arc_details[1][3]),
+            (arc_details[1][0]+arc_details[1][2],
+             arc_details[1][1]+arc_details[1][3]),
+            (arc_details[1][0]+arc_details[1][2], arc_details[1][1]),
+        ]
+        pygame.draw.lines(self.screen, arc_details[0], True, points, 1)
+        pygame.draw.arc(self.screen, *arc_details, AWT)
 
     def draw_arcs_out(self):
-        # t_rect =
-        # pygame.draw.rect(self.screen, (0, 0, 255), t_rect)
-
         try:
-            pygame.draw.arc(self.screen, RED,
-                            next(self.arc_gen_iv), PI/2, PI, AWT)
+            self.draw_single_arc(self.arc_gen_iv)
+            self.draw_single_arc(self.arc_gen_i)
+            self.draw_single_arc(self.arc_gen_ii)
+            self.draw_single_arc(self.arc_gen_iii)
 
-            pygame.draw.arc(self.screen, GREEN, next(self.arc_gen_i), 0, PI/2, AWT)
-
-            pygame.draw.arc(self.screen, RED, next(
-                self.arc_gen_ii), 3*PI/2, 2*PI, AWT)
-
-            pygame.draw.arc(self.screen, GREEN, next(
-                self.arc_gen_iii), PI, 3*PI/2, AWT)
-
-            pygame.draw.arc(self.screen, GREEN, next(
-                self.arc_gen_right), 1.75*PI, PI/4, AWT)
-            pygame.draw.arc(self.screen, GREEN, next(
-                self.arc_gen_left), 0.75*PI, 1.25*PI, AWT)
-
-
-
-            pygame.draw.arc(self.screen, RED, next(
-                self.box_gen_i), *next(self.rad_gen_i), AWT)
+            self.draw_single_arc(self.arc_gen_right)
+            self.draw_single_arc(self.arc_gen_left)
 
         except StopIteration:
             self.pinging = False
-            self.reset_arc_gen()
+            self.reset_empty_ping_arcs()
             logger.debug("Arc generators exhausted.")
-
 
     def draw(self):
         if self.pinging:
             self.draw_arcs_out()
-
 
     def start_ping(self):
         # create generator, give main loop arc drawing object and sound player
