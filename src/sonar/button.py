5fe1f0e60f138b033bf7275dd4db1561447ff5bc
UnicycleDumpTruck <junk@jeffhighsmith.com>
2022-08-20 16:38:41 -0400

    added debouncer. untested.

--- src/sonar/button.py
+++ src/sonar/button.py
@@ -7,6 +7,7 @@ from digitalio import Direction
 from digitalio import Pull
 from digitalio import DigitalInOut
 from adafruit_mcp230xx.mcp23017 import MCP23017
+from adafruit_debouncer import Debouncer
 
 i2c = busio.I2C(board.SCL, board.SDA)
 mcp = MCP23017(i2c)
@@ -14,32 +15,54 @@ mcp = MCP23017(i2c)
 
 class ButtonMgr():
     def __init__(self):
-
+        # Pins 0-7 on the right side of the MCP23017
         pin0 = mcp.get_pin(pin=0)
         pin0.switch_to_input()
         pin0.pull = Pull.UP
+        but0 = Debouncer(pin0)
+
         pin1 = mcp.get_pin(pin=1)
         pin1.switch_to_input()
         pin1.pull = Pull.UP
+        but1 = Debouncer(pin1)
+
         pin2 = mcp.get_pin(pin=2)
         pin2.switch_to_input()
         pin2.pull = Pull.UP
+        but2 = Debouncer(pin2)
+
         pin3 = mcp.get_pin(pin=3)
         pin3.switch_to_input()
         pin3.pull = Pull.UP
+        but3 = Debouncer(pin3)
+
         pin4 = mcp.get_pin(pin=4)
         pin4.switch_to_input()
         pin4.pull = Pull.UP
+        but4 = Debouncer(pin4)
+
         pin5 = mcp.get_pin(pin=5)
         pin5.switch_to_input()
         pin5.pull = Pull.UP
+        but5 = Debouncer(pin5)
+
         pin6 = mcp.get_pin(pin=6)
         pin6.switch_to_input()
         pin6.pull = Pull.UP
+        but6 = Debouncer(pin6)
+
         pin7 = mcp.get_pin(pin=7)
         pin7.switch_to_input()
         pin7.pull = Pull.UP
+        but7 = Debouncer(pin7)
 
+        pinGrn = DigitalInOut(board.D23)
+        butGrn = Debouncer(pinGrn)
+
+        pinRed = DigitalInOut(board.D24)
+        butRed = Debouncer(pinRed)
+
+        # Pins 8-15 on the left side of the MCP23017
         pin8 = mcp.get_pin(pin=8)
         pin8.direction = Direction.OUTPUT
         pin9 = mcp.get_pin(pin=9)
@@ -57,11 +80,8 @@ class ButtonMgr():
         pin15 = mcp.get_pin(pin=15)
         pin15.direction = Direction.OUTPUT
 
-        pinGrn = DigitalInOut(board.D23)
-        pinRed = DigitalInOut(board.D24)
-
-        self.inputs = [pin0, pin1, pin2, pin3, pin4,
-                       pin5, pin6, pin7, pinGrn, pinRed, ]
+        self.inputs = [but0, but1, but2, but3, but4,
+                       but5, but6, but7, butGrn, butRed, ]
         self.outputs = [pin8, pin9, pin10, pin11, pin12, pin13, pin14, pin15, ]
 
         self.input_states = [True for _ in range(10)]
@@ -69,11 +89,14 @@ class ButtonMgr():
     def update(self):
         changes = []
         for i, button in enumerate(self.inputs):
-            current_state = button.value
-            if current_state != self.input_states[i]:
-                logger.debug(f"Input {i} changed to {current_state}")
-                self.input_states[i] = current_state
+            button.update()
+            if button.fell:
+                logger.debug(f"Input {i} fell.")
                 changes.append(i)
+                if i < 8:
+                    self.outputs[i].value = False
+            if button.rose and i < 8:
+                self.outputs[i].value = False
         return changes
 
         # if i < 8:
